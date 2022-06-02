74af34b92e5996851c4c588b6085a408c2344d63
Rolly Mbaki <rollymbakii@gmail.com>
2022-06-02 16:24:29 +0200

    gewerkt aan game.ts

--- routes/game.ts
+++ routes/game.ts
@@ -1,11 +1,21 @@
 const express = require('express')
 const gameRouter = express.Router()
 const axios = require('axios')
+const FavClub = require('../models/favClub')
 
 gameRouter.get("/game", async(req: any, res:any) => {
   
   res.render("game");
 });
+
+gameRouter.get("/favclub", (req: any, res:any)=>{
+  
+  FavClub.find()
+    .then((result:any) => {
+      res.send(result)
+    })
+    .catch((err:any)=> console.log(err))
+})
 
 export{}
 module.exports = gameRouter
